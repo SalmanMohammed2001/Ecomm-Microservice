119cf1dac46f6bd747b0a0ce97ca60478fda41d2
salman <salman@rapidm3.com>
2025-05-12 00:26:46 +0530

    CategorySchema.js impl

---
+++ product-service-api/model/CategorySchema.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose')
+
+const  CategorySchema=new mongoose.Schema({
+  categoryName:{
+        type:String,
+        required:true
+    },
+    icon:{
+        type:Object
+    },
+    availableCountries:{
+        type:Array
+    }
+})
+
+module.exports=mongoose.model('category',CategorySchema)
